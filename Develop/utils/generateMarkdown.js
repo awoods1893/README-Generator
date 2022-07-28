762d266b9c83860428083eef414a212f54b62ce0
awoods1893 <awoods1983@gmail.com>
2022-07-27 22:50:21 -0500

    added the generateMarkdown.js code for entering data into node

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,37 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `## ${data.title}
+        # Table of Contents
+        - [Project Description](#project-description)
+        - [Usage](#usage)
+        - [Tests](#tests)
+        - [Installation](#installation)
+        - [License](#license)
+        - [Contributions](#contributions)
+        - [Questions](#questions)
+
+        # Project Description
+        ${data.description}
+
+        # Usage
+        ${data.usage}
+
+        # Tests
+        ${data.tests}
+
+        # Installation
+        ${data.installation}
+
+        # License
+        ${data.license}
+
+        # Contributions
+        ${data.contributions}
+
+        # Questions
+        Questions? Please contact the project owner at #### ${data.email}
+
 
 `;
 }
